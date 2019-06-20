5298ece78aea95431359b4cf474342f35113a4d6
Mustafa Karademir <mustafa.karademir@develappers.de>
2019-06-20 09:21:40 +0200

    sidemenu menu points added

--- HTWDD/Components/SideMenu/Main/SideMenuViewController.swift
+++ HTWDD/Components/SideMenu/Main/SideMenuViewController.swift
@@ -10,17 +10,46 @@ import Foundation
 
 class SideMenuViewController: UIViewController {
     
+    // MARK: IBOutlet
     @IBOutlet weak var classMenuButton: UIButton!
     @IBOutlet weak var examsMenuButton: UIButton!
     @IBOutlet weak var gradesMenuButton: UIButton!
     @IBOutlet weak var canteenMenuButton: UIButton!
     @IBOutlet weak var settingsMenuButton: UIButton!
+    @IBOutlet var menuButtons: [UIButton]!
+    
+    
+    // MARK: Properties
+    private let defaultTintColor: UIColor = .darkGray
+    private let selectedTintColor: UIColor = .blue
     
     override func viewDidLoad() {
-        classMenuButton.setTitle(Loca.Schedule.title, for: .normal)
-        examsMenuButton.setTitle(Loca.Exams.title, for: .normal)
-        gradesMenuButton.setTitle(Loca.Grades.title, for: .normal)
-        canteenMenuButton.setTitle(Loca.Canteen.title, for: .normal)
-        settingsMenuButton.setTitle(Loca.Settings.title, for: .normal)
+        prepareMenuButtons()
+    }
+    
+    fileprivate func prepareMenuButtons() {
+        
+        func setTitleAndTintColor(for button: inout UIButton, title: String, tintColor: UIColor = .darkGray) {
+            button.apply { b in
+                b.setTitle(title, for: .normal)
+                b.tintColor = tintColor
+            }
+        }
+        
+        setTitleAndTintColor(for: &classMenuButton, title: Loca.Schedule.title)
+        setTitleAndTintColor(for: &examsMenuButton, title: Loca.Exams.title)
+        setTitleAndTintColor(for: &gradesMenuButton, title: Loca.Grades.title)
+        setTitleAndTintColor(for: &canteenMenuButton, title: Loca.Canteen.title)
+        setTitleAndTintColor(for: &settingsMenuButton, title: Loca.Settings.title)
+    }
+    
+    @IBAction func onMenuButtonTouchUpInside(_ sender: UIButton) {
+        menuButtons.filter { $0 != sender }.forEach { (button) in
+            button.tintColor = defaultTintColor
+        }
+        
+        sender.apply {
+            $0.tintColor = selectedTintColor
+        }
     }
 }
