45bb5dc8bde08cecd986c08f2a3843c52f2f2165
Mustafa Karademir <mustafa.karademir@develappers.de>
2019-07-03 10:29:11 +0200

    Removed pods folder from git

--- Pods/Target Support Files/Pods-HTWDD Today/Pods-HTWDD Today-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HTWDD_Today : NSObject
-@end
-@implementation PodsDummy_Pods_HTWDD_Today
-@end
--- Pods/Target Support Files/Pods-HTWDD/Pods-HTWDD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HTWDD : NSObject
-@end
-@implementation PodsDummy_Pods_HTWDD
-@end
