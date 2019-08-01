e38ec7bd5f1fac3f3828b105c5f47a762e2fdd0a
Mustafa Karademir <mustafa.karademir@develappers.de>
2019-08-01 14:35:43 +0200

    + Onboarding.Analytics

--- HTWDD/Components/Onboarding/Main/AnalyticsViewController.swift
+++ HTWDD/Components/Onboarding/Main/AnalyticsViewController.swift
@@ -13,8 +13,11 @@ class AnalyticsViewController: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet weak var lblAnalyticsDescription: UILabel!
+    @IBOutlet weak var lblAnalyticsInformation: UILabel!
     @IBOutlet weak var lblAnalyticsRevokeDescription: UILabel!
     @IBOutlet weak var animationView: AnimationView!
+    @IBOutlet weak var lblHelpQuestion: UILabel!
+    @IBOutlet weak var btnYes: UIButton!
     
     // MARK: - Properties
     private lazy var animation: Animation? = {
@@ -26,11 +29,7 @@ class AnalyticsViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        animationView.apply {
-            $0.animation    = animation
-            $0.contentMode  = .scaleAspectFit
-            $0.loopMode     = .loop
-        }
+        setup()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -40,7 +39,33 @@ class AnalyticsViewController: UIViewController {
         }
     }
     
+    // MARK: - User Interaction
     @IBAction func onYesTouch(_ sender: UIButton) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            
+            self.animation                  = Animation.named("PulseGrey")
+            self.animationView.animation    = self.animation
+            self.btnYes.isEnabled           = false
+        }
         delegate?.next(animated: true)
     }
+}
+
+// MARK: - Setup
+extension AnalyticsViewController {
+    
+    private func setup() {
+        lblAnalyticsDescription.text        = R.string.localizable.onboardingAnalyticsDescription()
+        lblAnalyticsInformation.text        = R.string.localizable.onboardingAnalyticsInformation()
+        lblAnalyticsRevokeDescription.text  = R.string.localizable.onboardingAnalyticsRevokeDescription()
+        lblHelpQuestion.text                = R.string.localizable.onboardingAnalyticsHelpQuestion()
+        btnYes.titleLabel?.text             = R.string.localizable.yes()
+        animationView.apply {
+            $0.animation    = animation
+            $0.contentMode  = .scaleAspectFit
+            $0.loopMode     = .loop
+        }
+    }
+    
 }
