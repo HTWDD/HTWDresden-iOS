7393105cf63638f416d5df25d6510c922bc17476
Christian Herlemann <christian.herlemann@develappers.de>
2021-06-08 18:10:03 +0200

    Adding search field for elective lesson selection

--- HTWDD/Components/Timetable/Main/TimetableElectiveLessonSelection/TimetableElectiveLessonSelectionViewController.swift
+++ HTWDD/Components/Timetable/Main/TimetableElectiveLessonSelection/TimetableElectiveLessonSelectionViewController.swift
@@ -17,15 +17,30 @@ class TimetableElectiveLessonSelectionViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var viewModel: TimetableElectiveLessonSelectionViewModel!
+    let searchController = UISearchController(searchResultsController: nil)
     
-    private var electiveLessons = [Lesson](){
+    private var filteredElectiveLessons = [Lesson]()
+    private var electiveLessons = [Lesson]() {
         didSet {
+            filteredElectiveLessons = electiveLessons
             tableView.reloadData()
         }
     }
     
     override func viewDidLoad() {
         self.title = "Wähle eine Veranstaltung"
+        
+        if #available(iOS 13.0, *) {
+            searchController.searchResultsUpdater = self
+            searchController.obscuresBackgroundDuringPresentation = false
+            searchController.searchBar.placeholder = "Suchen"
+            searchController.searchBar.searchTextField.backgroundColor = .white
+            
+            navigationItem.searchController = searchController
+            definesPresentationContext = true
+            
+            searchController.searchBar.delegate = self
+        }
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -46,29 +61,62 @@ class TimetableElectiveLessonSelectionViewController: UIViewController {
                 Log.error(error)
             }).disposed(by: self.rx_disposeBag)
     }
-
+    
     @objc func close() {
         self.dismiss(animated: true)
+    }
+    
+    func filterContentForSearchText(_ searchText: String?) {
+        guard let searchText = searchText, searchText.count > 0 else {
+            filteredElectiveLessons = electiveLessons
+            tableView.reloadData()
+            return
+        }
+        
+        filteredElectiveLessons = electiveLessons.filter { (lesson: Lesson) -> Bool in
+            let nameFilter = lesson.name.lowercased().contains(searchText.lowercased())
+            let professorFilter = (lesson.professor ?? "").lowercased().contains(searchText.lowercased())
+            var dayFilter = false
+            if let day = CalendarWeekDay(rawValue: lesson.day) {
+                dayFilter = day.localizedDescription.lowercased().contains(searchText.lowercased())
+            }
+            
+            return  nameFilter || professorFilter || dayFilter
+        }
+        tableView.reloadData()
     }
 }
 
 extension TimetableElectiveLessonSelectionViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.electiveLessons.count
+        return self.filteredElectiveLessons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(TimetableElectiveLessonCell.self, for: indexPath)!
-        cell.setup(lesson: electiveLessons[indexPath.row])
+        cell.setup(lesson: filteredElectiveLessons[indexPath.row])
         return cell
     }
 }
 
 extension TimetableElectiveLessonSelectionViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.viewModel.saveElectiveLesson(selected: electiveLessons[indexPath.row])
+        self.viewModel.saveElectiveLesson(selected: filteredElectiveLessons[indexPath.row])
         
         self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension TimetableElectiveLessonSelectionViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchBar.text)
+    }
+}
+
+extension TimetableElectiveLessonSelectionViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text)
     }
 }
